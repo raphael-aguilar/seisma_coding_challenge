0f6aabf8d1f27b09b130656b5c1f41c7ecc9c3bc
raphael-aguilar <raphaeljaguilar@gmail.com>
2021-08-08 13:53:42 +1000

    Initial Setup

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+//        Read in CSV
+
+
+//        Calculate data for line
+
+//        Put line to output csv
+
+
+
+    }
+}
